972893f32d1d6f299304c6610879350cfce0ff8a
Rana Ali <ar5669034@gmail.com>
2026-02-05 21:41:59 +0500

    Create StudentData.cpp

---
+++ StudentData.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main() {
+    string name, department;
+    int rollNumber;
+    cout << "Enter Name: ";
+    cin >> name ;
+    cout << "Enter Roll Number: ";
+    cin >> rollNumber;
+    cout << "Enter Department: ";
+    cin >> department ;
+    
+    return 0;
+}
